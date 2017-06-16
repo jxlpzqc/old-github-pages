b224e4450dd05c7b1d8098502525041254b96a40
jxlpzqc <jxlpzqc@live.com>
2017-06-16 17:19:03 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+	// Header Scroll
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 50) {
+			$('#header').addClass('fixed');
+		} else {
+			$('#header').removeClass('fixed');
+		}
+	});
+
+	// Waypoints
+	$('.work').waypoint(function() {
+		$('.work').addClass('animated fadeIn');
+	}, {
+		offset: '75%'
+	});
+	$('.download').waypoint(function() {
+		$('.download .btn').addClass('animated tada');
+	}, {
+		offset: '75%'
+	});
+    //fancybox
+//	var result = window.matchMedia('(max-width: 700px)');
+//
+//    if (result.matches) {
+//      console.log('页面宽度小于等于700px');
+//    } else {
+//      console.log('页面宽度大于700px');
+//        $('.work-box').fancybox();
+//    }
+//	
+
+	// Flexslider
+	$('.flexslider').flexslider({
+		animation: "fade",
+		directionNav: false,
+	});
+
+	// Page Scroll
+	var sections = $('section')
+		nav = $('nav[role="navigation"]');
+
+	$(window).on('scroll', function () {
+	  	var cur_pos = $(this).scrollTop();
+	  	sections.each(function() {
+	    	var top = $(this).offset().top - 76
+	        	bottom = top + $(this).outerHeight();
+	    	if (cur_pos >= top && cur_pos <= bottom) {
+	      		nav.find('a').removeClass('active');
+	      		nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    	}
+	  	});
+	});
+	nav.find('a').on('click', function () {
+	  	var $el = $(this)
+	    	id = $el.attr('href');
+		$('html, body').animate({
+			scrollTop: $(id).offset().top - 75
+		}, 500);
+	  return false;
+	});
+
+	// Mobile Navigation
+	$('.nav-toggle').on('click', function() {
+		$(this).toggleClass('close-nav');
+		nav.toggleClass('open');
+		return false;
+	});	
+	nav.find('a').on('click', function() {
+		$('.nav-toggle').toggleClass('close-nav');
+		nav.toggleClass('open');
+	});
+    
+    
+});
+
+//
+//$(window).resize(function(){
+//    iFrameHeight();
+//})
+//
+//function iFrameHeight() { 
+//var ifm= document.getElementById("mainFrame"); 
+//var subWeb = document.frames ? document.frames["iframepage"].document : ifm.contentDocument; 
+//if(ifm != null && subWeb != null) { 
+//ifm.height = subWeb.body.scrollHeight; 
+//} 
+//    
+//} 
